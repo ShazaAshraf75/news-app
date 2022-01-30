b35c90e6c634c7e56d949fa89808e8d60585c904
Shaza Ashraf <Shazaashraf75@gmail.com>
2022-01-30 14:21:54 +0300

    Remove Comment

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,6 @@ class MyApp extends StatelessWidget {
         listener: (context, state) {},
         builder: (context, state) {
           return MaterialApp(
-            //shaza ashraf mohammad
             debugShowCheckedModeBanner: false,
             theme: newsLightTheme,
             darkTheme: newsDarkTheme,
